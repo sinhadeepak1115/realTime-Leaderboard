40030072ec9e06d0997779783fa238a1a18a234f
Deepak Kumar Sinha <94794077+sinhadeepak1115@users.noreply.github.com>
2025-07-05 18:44:23 +0530

    basic ws server

--- websocket/src/index.ts
+++ websocket/src/index.ts
@@ -1,12 +1,16 @@
-import WebSocket from "ws";
+import WebSocket, { WebSocketServer } from "ws";
 
-const ws = new WebSocket("ws://localhost:8080");
+const wss = new WebSocketServer({ port: 8080 });
 
-ws.on("error", console.error);
-
-ws.on("open", () => {
-  function open() {
-    console.log("WebSocket connection opened");
-    ws.send("Hello, server!");
-  }
+wss.on("connection", (ws) => {
+  ws.on("error", console.error);
+  ws.on("message", (data, isBinary) => {
+    wss.clients.forEach((client) => {
+      if (client !== ws && client.readyState === WebSocket.OPEN) {
+        client.send(data, { binary: isBinary });
+      }
+    });
+  });
 });
+
+console.log("WebSocket server is running on ws://localhost:8080");
